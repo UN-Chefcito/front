ee97b37d3bdcac134188d3ead07374efe76d3f5a
Camilo Ayala <camiloaayala6@gmail.com>
2023-10-16 12:29:00 -0500

    Added color schema

---
+++ lib/core/constants/colors.dart
@@ -0,0 +1,8 @@
+import 'dart:ui';
+
+const Color primary = Color(0xFF59738C);
+const Color white = Color(0xFFFFFFFF);
+const Color black = Color(0xFF000000);
+const Color secondary = Color(0xFF7F9AB5);
+const Color grey = Color(0xFFC5C5C5);
+const Color warning = Color(0xFFD79587);
